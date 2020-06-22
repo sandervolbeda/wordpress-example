9a81cfed0c97958e04ff650cea71544eca6cf8ef
Sander Volbeda <hallo@sandervolbeda.com>
2020-06-22 11:30:10 +0200

    Create taxonomy.php

---
+++ cpt+tax/taxonomy.php
@@ -0,0 +1,31 @@
+<?php
+
+// Let us create Taxonomy for Custom Post Type
+add_action( 'init', 'crunchify_create_deals_custom_taxonomy', 0 );
+ 
+//create a custom taxonomy name it "type" for your posts
+function crunchify_create_deals_custom_taxonomy() {
+ 
+  $labels = array(
+    'name' => _x( 'Types', 'taxonomy general name' ),
+    'singular_name' => _x( 'Type', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Types' ),
+    'all_items' => __( 'All Types' ),
+    'parent_item' => __( 'Parent Type' ),
+    'parent_item_colon' => __( 'Parent Type:' ),
+    'edit_item' => __( 'Edit Type' ), 
+    'update_item' => __( 'Update Type' ),
+    'add_new_item' => __( 'Add New Type' ),
+    'new_item_name' => __( 'New Type Name' ),
+    'menu_name' => __( 'Types' ),
+  ); 	
+ 
+  register_taxonomy('types',array('deals'), array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'type' ),
+  ));
+}
